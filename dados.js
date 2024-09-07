43c61df5f8e4cb6bb83f6d8217724d57d3502e71
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2024-09-06 21:25:06 -0300

    test: acompanhando a aula 03 - Criando a base de dados com JavaScript

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+  {
+    titulo: "Rayssa Leal",
+    descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+  },
+  {
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira que vem se destacando no cenário internacional.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+  },
+  {
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros com seu talento e determinação.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+  }
+];
+
